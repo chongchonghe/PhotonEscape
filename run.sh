3dd8d19852e18146c8e780854fde80f2e8c2c346
ChongChong He <chongchonghe99@gmail.com>
2024-10-18 16:25:20 +1100

    add run.sh as a test run over Fred's data

---
+++ run.sh
@@ -0,0 +1,5 @@
+python loop_fred_sims.py process data/cluster_sfe_70 --subsample 0.01 --dist 1_kpc --refine 2
+python loop_fred_sims.py fesc data/cluster_sfe_70 --outdir outs/sfe70/age30Myr
+
+# # more:
+# python loop_fred_sims.py fesc data/cluster_sfe_70 --outdir outs/sfe70/age1000Myr
